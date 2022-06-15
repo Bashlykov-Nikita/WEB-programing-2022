7b37d6e260c4c2f8548151f8763ee16b34b9b738
Nikita <bashlykov75@gmail.com>
2022-06-15 15:50:55 +0300

    without clipboard

--- scripts.js
+++
@@ -1,8 +0,0 @@
-var clipboard = new ClipboardJS('.mail_copy');
-var myToolTip = document.getElementById("myTooltip");
-var mailCcopy = document.getElementByClassName("mail_copy")
-
-$(".mail-copy").onclick(function(){
-    let MyTooltip =document.getElementById("myTooltip");
-    MyTooltip.textContent = "Email copied";
-});
